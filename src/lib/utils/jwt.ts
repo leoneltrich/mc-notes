6ba51756bcaff0f30388f942a54db241489c9dae
leoneltrich <leoneltrich@icloud.com>
2026-03-01 20:16:03 +0100

    Added jwt expiration parsing

---
+++ src/lib/utils/jwt.ts
@@ -0,0 +1,15 @@
+export function getJwtExpiration(token: string): number | null {
+  try {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    const payload = JSON.parse(jsonPayload);
+    return payload.exp || null;
+  } catch (e) {
+    console.error('Failed to parse JWT', e);
+    return null;
+  }
+}
